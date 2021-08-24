a7c7f4bded81960eeaca56bc05fc11ccfe546320
pvcapuano <pvcapuano@gmail.com>
2021-08-23 23:02:54 -0300

    js final

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // let banco = []
 
 const getBanco = () => JSON.parse(localStorage.getItem('todoList')) ?? []
+const setBanco = (banco) => localStorage.setItem('todoList', JSON.stringify(banco))
 
 const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label')
@@ -31,19 +32,25 @@ const inserirItem = (evento) => {
     const texto = evento.target.value
 
     if (tecla === 'Enter'){
-        
+        const banco = getBanco()
         banco.push({'tarefa': texto, 'status': ''})
+        setBanco(banco)
         atualizarTela()
         evento.target.value = ''
     }
 }
 const removerItem = (indice) => {
+    const banco = getBanco()
     banco.splice(indice,1)
+    setBanco(banco)
     atualizarTela()
 }
 
 const atualizarItem = (indice) => {
+    const banco = getBanco()
     banco[indice].status = banco[indice].status === '' ? 'checked' : ''
+    setBanco(banco)
+    atualizarTela()
 } 
 const clickItem = (evento) => {
     const elemento = evento.target
